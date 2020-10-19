7b52eb18056bf8c8cf79718c5c17e3cb2ad05784
joelcarrillo <joel.carrillo.pa@gmail.com>
2020-10-19 17:38:25 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,121 @@
+const calculator = document.querySelector(".calculator");
+let stack = [];
+let operationsOrder = [];
+
+calculator.addEventListener("click", function (event) {
+  const element = event.target;
+  if (element.classList.contains("btn")) {
+    const number = parseInt(element.innerText);
+    if (isNaN(number)) {
+      if (element.innerText === "÷") {
+        pushToStack();
+        stack.push("/");
+        clearScreen();
+      } else if (element.innerText === "×") {
+        pushToStack();
+        stack.push("*");
+        clearScreen();
+      } else if (element.innerText === "−") {
+        pushToStack();
+        stack.push("-");
+        clearScreen();
+      } else if (element.innerText === "+") {
+        pushToStack();
+        clearScreen();
+        stack.push("+");
+        clearScreen();
+      } else if (element.innerText === "=") {
+        pushToStack();
+        resolveStack();
+      } else if (element.innerText === "C") {
+        stack.splice(0, stack.length);
+        clearScreen();
+        console.clear();
+      } else if (element.innerText === "←") {
+        removeNumber();
+      }
+    } else {
+      displayOnScreen(number);
+    }
+  }
+  console.log(stack);
+});
+
+function pushToStack() {
+  let number = parseInt(document.querySelector(".numbers").innerText);
+  stack.push(number);
+}
+
+function resolveStack() {
+  console.log("_____________________________________");
+  console.log(stack);
+  while (stack.includes("*") || stack.includes("/")) {
+    for (let index = 0; index < stack.length; index++) {
+      if (stack[index] === "*") {
+        stack[index] = stack[index - 1] * stack[index + 1];
+        cutStack(index);
+        console.log(stack);
+        break;
+      } else if (stack[index] === "/") {
+        stack[index] = stack[index - 1] / stack[index + 1];
+        cutStack(index);
+        console.log(stack);
+        break;
+      }
+    }
+  }
+  while (stack.includes("+") || stack.includes("-")) {
+    for (let index = 0; index < stack.length; index++) {
+      if (stack[index] === "+") {
+        stack[index] = stack[index - 1] + stack[index + 1];
+        cutStack(index);
+        console.log(stack);
+        break;
+      } else if (stack[index] === "-") {
+        stack[index] = stack[index - 1] - stack[index + 1];
+        cutStack(index);
+        console.log(stack);
+        break;
+      }
+    }
+  }
+  clearScreen();
+  displayOnScreen(stack.toString());
+  stack.splice(0, stack.length);
+}
+
+function displayOnScreen(number) {
+  let screen = document.querySelector(".numbers");
+
+  if (screen.innerText === "0") {
+    screen.innerText = number;
+  } else {
+    screen.innerText += number;
+  }
+}
+
+function cutStack(index) {
+  stack.splice(index - 1, 1);
+  stack.splice(index, 1);
+}
+
+function clearScreen() {
+  let screen = document.querySelector(".numbers");
+  screen.innerText = 0;
+}
+
+function removeNumber() {
+  numberElement = document.querySelector(".numbers");
+  numberOnDisplay = document.querySelector(".numbers").innerText;
+
+  if (numberOnDisplay === "0") {
+    return;
+  } else if (numberOnDisplay.length === 1) {
+    numberElement.innerText = 0;
+  } else {
+    numberElement.innerText = numberOnDisplay.slice(
+      0,
+      numberOnDisplay.length - 1
+    );
+  }
+}
